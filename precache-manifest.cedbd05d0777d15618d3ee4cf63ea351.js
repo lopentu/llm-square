019a25a4b3e8ad7a9324c45e4268891872bbc90d
lowannann <jajoanne.chi88@gmail.com>
2024-04-10 17:56:23 +0800

    Updates

---
+++ precache-manifest.cedbd05d0777d15618d3ee4cf63ea351.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdfb49941730253f57bd5f4759da60e6",
+    "url": "/llm-square/index.html"
+  },
+  {
+    "revision": "0b7295675cabcf1ade0a",
+    "url": "/llm-square/static/css/main.cf0742fd.chunk.css"
+  },
+  {
+    "revision": "3225b595ba086434c230",
+    "url": "/llm-square/static/js/2.a2a620a9.chunk.js"
+  },
+  {
+    "revision": "3478b494b1c1800ea949f65ca5135346",
+    "url": "/llm-square/static/js/2.a2a620a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b7295675cabcf1ade0a",
+    "url": "/llm-square/static/js/main.3148a712.chunk.js"
+  },
+  {
+    "revision": "8b1f7bef2acd57e574c6",
+    "url": "/llm-square/static/js/runtime-main.d788c632.js"
+  },
+  {
+    "revision": "0aefec326f9babd35d6e85f0c427f9e3",
+    "url": "/llm-square/static/media/Entailment.0aefec32.png"
+  },
+  {
+    "revision": "3b27edbfd0b6c0f0e5a323f4ab21020e",
+    "url": "/llm-square/static/media/data_comparison.3b27edbf.png"
+  },
+  {
+    "revision": "68314b5c56d4daebe981f7ae1d441fc8",
+    "url": "/llm-square/static/media/data_curation.68314b5c.png"
+  },
+  {
+    "revision": "8d757e482666171d62d98169c0c0411d",
+    "url": "/llm-square/static/media/llm_logo.8d757e48.png"
+  },
+  {
+    "revision": "2b18354836776bcc0edc5272ee43ec13",
+    "url": "/llm-square/static/media/llm_studying.2b183548.png"
+  },
+  {
+    "revision": "54bfe818fb69c34b27f764a455137ce0",
+    "url": "/llm-square/static/media/main-project.54bfe818.png"
+  },
+  {
+    "revision": "e82dfd9bd2755bba27cfaa49e998e293",
+    "url": "/llm-square/static/media/ntu.e82dfd9b.png"
+  },
+  {
+    "revision": "559fb4320e70038d65af4f2853b413fe",
+    "url": "/llm-square/static/media/nvidia.559fb432.png"
+  },
+  {
+    "revision": "cab6a3e741642b96a1bcaec7f6ac4476",
+    "url": "/llm-square/static/media/shukai.cab6a3e7.png"
+  },
+  {
+    "revision": "c911b8368a89768e17fec67e4e29492c",
+    "url": "/llm-square/static/media/typing_cat.c911b836.png"
+  },
+  {
+    "revision": "7ab53d3c620d1fc628e3c61d62ccab29",
+    "url": "/llm-square/static/media/verdict.7ab53d3c.png"
+  },
+  {
+    "revision": "d415b5d936ba1e6fcfec5a3f325d5e2e",
+    "url": "/llm-square/static/media/workflow.d415b5d9.png"
+  }
+]);
